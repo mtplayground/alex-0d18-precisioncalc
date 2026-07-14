af514380486da11f37369ff4f88cb21bd69e3263
myclawteam-ci <ci@myclawteam.ai>
2026-07-14 22:17:15 +0000

    feat: Build expression parser/evaluator with standard operator precedence

---
+++ src/lib/calculation/index.ts
@@ -0,0 +1,19 @@
+export {
+  evaluateExpression,
+  evaluateParsedExpression,
+  parseExpression,
+  tokenizeExpression,
+} from './expression';
+
+export type {
+  ArithmeticOperator,
+  BinaryExpressionNode,
+  EvaluationResult,
+  ExpressionError,
+  ExpressionErrorCode,
+  ExpressionNode,
+  NumberNode,
+  ParseExpressionResult,
+  UnaryExpressionNode,
+  UnaryOperator,
+} from './expression';
